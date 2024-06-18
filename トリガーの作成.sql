17a69668a2b39331b8c425eacf331b3c7e2fd37e
HaNabEE02 <hanabee0216@gmail.com>
2024-06-18 23:04:10 +0900

    Update トリガーの作成.sql
    韓国語を英語に変更

--- トリガーの作成.sql
+++ トリガーの作成.sql
@@ -1,4 +1,4 @@
--- USER가 피시방 시간을 충전했을 때, 해당 USER의 CHARGED_TIME이 충전시간만큼 변경되는 트리거
+-- When the USER charges the PC room time, triggering that the CHARGED_TIME of that USER changes by the charging time
 CREATE TRIGGER UP_USER_TIME
 ON ORDERS
 AFTER INSERT
